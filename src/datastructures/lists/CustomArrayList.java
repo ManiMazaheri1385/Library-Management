1d340bc5a818052037d1f08139a030902b47db10
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-05-06 00:48:55 +0330

    complete custom array list class

--- src/datastructures/lists/CustomArrayList.java
+++ src/datastructures/lists/CustomArrayList.java
@@ -19,6 +19,74 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) elements[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        elements[index] = element;
+        return element;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(Object object) {
+        if(object == null) {
+            throw new NullPointerException();
+        }
+        ensureCapacity();
+        size++;
+        elements[size - 1] = object;
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        int i = 0;
+        for (; i < size; i++) {
+            if (elements[i].equals(object)) {
+                break;
+            }
+        }
+        return i != size;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        if(object == null) {
+            throw new NullPointerException();
+        }
+        int i = 0;
+        for (; i < size; i++) {
+            if (elements[i].equals(object)) {
+                break;
+            }
+        }
+        if (i == size) {
+            return false;
+        }
+        for (; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return true;
+    }
 
 }
